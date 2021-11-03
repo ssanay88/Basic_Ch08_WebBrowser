fa4460aee20f641aa4c39409e7e22ce2265c63da
Yangsuhyuck <pplloo748@gmail.com>
2021-11-03 19:03:06 +0900

    패스트 캠퍼스 Basic-Ch8-웹 브라우저

---
+++ app/src/main/java/com/example/basic_ch08_webbrowser/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.basic_ch08_webbrowser
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.basic_ch08_webbrowser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        initViews()
+        bindViews()
+
+    }
+
+
+    override fun onBackPressed() {
+
+        if (mainBinding.webView.canGoBack()) {  // 뒤로 갈 수 있는 경우에 뒤 히스토리로 이동동
+           mainBinding.webView.goBack()
+        } else {
+            super.onBackPressed()   // 뒤로가기 누를시 앱이 종료
+        }
+
+
+
+    }
+    private fun initViews() {
+        mainBinding.webView.apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            settings.javaScriptEnabled = true
+            loadUrl(HOMEURL)
+        }
+
+    }
+
+    private fun bindViews() {
+        // EditText에서 액션 처리 view : 텍스트가 입력 되는 곳 , i : 발생하는 액션 구분 ,
+        mainBinding.addressBar.setOnEditorActionListener { view, i, keyEvent ->
+
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                val loadUrl = view.text.toString()
+                // http나 https가 붙는지 확인해주는 함수 둘 중 아무것도 없을 경우 false 반환
+                if (URLUtil.isNetworkUrl(loadUrl)) {
+                    mainBinding.webView.loadUrl(loadUrl)
+                } else {
+                    mainBinding.webView.loadUrl("http://$loadUrl")
+                }
+
+
+        }
+            return@setOnEditorActionListener false
+
+        }
+
+        mainBinding.backBtn.setOnClickListener {
+            mainBinding.webView.goBack()    // 이전 히스토리로 넘어간다
+        }
+
+        mainBinding.forwardBtn.setOnClickListener {
+            mainBinding.webView.goForward()
+        }
+
+        mainBinding.homeBtn.setOnClickListener {
+            mainBinding.webView.loadUrl(HOMEURL)
+        }
+
+        mainBinding.refreshLayout.setOnRefreshListener {
+            mainBinding.webView.reload()
+        }
+
+
+    }
+
+    // 내부 클래스로 선언해야 상위 클래스에 접근이 가능
+    inner class WebViewClient: android.webkit.WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            mainBinding.progressBar.show()  // 페이지 시작할때 progressBar 나타남
+        }
+
+        // 페이지가 로딩이 끝났을 경우
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            // 로딩이 끝났을 경우 false로 만들어 줘야 갱신중이라는 UI가 끝이난다
+            mainBinding.refreshLayout.isRefreshing = false
+            mainBinding.progressBar.hide()    // 페이지 로딩이 끝날경우 숨김
+            mainBinding.backBtn.isEnabled = mainBinding.webView.canGoBack()    // 뒤로 갈 수 있는 경우(True)일 경우만 버튼 활성화
+            mainBinding.forwardBtn.isEnabled = mainBinding.webView.canGoForward()    // 앞으로 갈 수 있는 경우(True)일 경우만 버튼 활성화
+            mainBinding.addressBar.setText(url)    // 최종적으로 로딩된 url로 바꿔준다
+
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient() {
+
+
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            mainBinding.progressBar.progress = newProgress
+
+        }
+    }
+
+    companion object {
+        private const val HOMEURL = "http://www.google.com"
+    }
+
+
+
+}
